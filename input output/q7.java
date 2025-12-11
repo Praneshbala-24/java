548fd6e132208dab18f9805036c8606108747c41
Pranesh Bala <praneshbala24@gmail.com>
2025-12-11 11:13:43 +0530

    Add files via upload

---
+++ input output/q7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class q7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.printf("%X", num);
+        sc.close();
+    }
+}
