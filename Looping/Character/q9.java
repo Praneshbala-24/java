9c27002e1e4aefdcc42e4cf238b67fab324f26c5
Pranesh Bala <praneshbala24@gmail.com>
2025-12-18 09:21:48 +0530

    Update q9.java

--- Looping/Character/q9.java
+++ Looping/Character/q9.java
@@ -2,17 +2,10 @@ import java.util.Scanner;
 public class q9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a decimal number: ");
-        int num = sc.nextInt();
-        int binary = 0;
-        int place = 1;
-        while (num > 0) {
-            int rem = num % 2;
-            binary = binary + rem * place;
-            place = place * 10;
-            num = num / 2;
-        }
-        System.out.println("Binary equivalent = " + binary);
-        sc.close();
+        System.out.print("Enter a binary number: ");
+        String binaryString = sc.next();
+        int decimal = Integer.parseInt(binaryString, 2);
+        System.out.println("Binary: " + binaryString);
+        System.out.println("Decimal: " + decimal);
     }
 }
