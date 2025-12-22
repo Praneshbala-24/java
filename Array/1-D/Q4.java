843dee7cc975b4416ff473bcb5922b9d3bc7af63
Pranesh Bala <praneshbala24@gmail.com>
2025-12-22 22:23:01 +0530

    Add files via upload

---
+++ Array/1-D/Q4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Q4_SumEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] % 2 == 0)
+                sum += arr[i];
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ Array/1-D/Q5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Q5_AlternateSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+            sum += (i % 2 == 0) ? arr[i] : -arr[i];
+
+        System.out.println(sum);
+    }
+}
